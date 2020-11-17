ebf9c44ed5c9d720fa5049021684c423a788f5ba
Jessie Hechavarria <jessie.hechavarria@gmail.com>
2020-11-17 10:08:01 +0100

    feat: setup initial structure

---
+++ app.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+###################################################
+#
+# File Name : app.sh
+# Created By : antrhaxx
+# Creation Date : 2020-11-17 00:02:21
+# Last Modified : 2020-11-17 00:25:04
+# Purpose : 
+#
+###################################################
+
+yarn
+
+yarn dev --hostname 0.0.0.0 -p 80
