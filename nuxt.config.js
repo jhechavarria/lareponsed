dd107caec17a67d11d900b07739a74e13d552cb6
Jessie Hechavarria <jessie.hechavarria@gmail.com>
2020-11-21 18:05:12 +0100

    feat: updates nuxt config

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,7 +14,8 @@
  * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
  */
 export default {
-  mode: 'spa',
+  ssr: false,
+  target: 'static',
   /*
   ** Headers of the page
   */
